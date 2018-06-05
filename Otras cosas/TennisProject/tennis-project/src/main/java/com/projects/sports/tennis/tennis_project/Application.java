5ec7a030a85d9d4cf50ffe1d7a7aefd86b41886c
emollar <emollar@leda-mc.com>
2018-06-05 12:18:23 +0200

    Comenzando el proyecto de tenis

---
+++ Otras cosas/TennisProject/tennis-project/src/main/java/com/projects/sports/tennis/tennis_project/Application.java
@@ -0,0 +1,13 @@
+package com.projects.sports.tennis.tennis_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
