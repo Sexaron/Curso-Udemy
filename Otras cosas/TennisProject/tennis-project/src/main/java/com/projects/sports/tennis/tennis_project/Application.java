4fbf341a84a2b470d131cbc13afcbd664bad1231
emollar <emollar@leda-mc.com>
2018-06-07 17:46:22 +0200

    continuamos

--- Otras cosas/TennisProject/tennis-project/src/main/java/com/projects/sports/tennis/tennis_project/Application.java
+++ Otras cosas/TennisProject/tennis-project/src/main/java/com/projects/sports/tennis/tennis_project/Application.java
@@ -6,11 +6,14 @@ public class Application
 	//VARIABLES
 	final static int NUM_PARTIDOS = 5; 
 	final static int NUM_SETS = 3;
+	static int puntoLCL;
+	static int puntoVST;
 	
-	public Match arrayPartidos[] = new Match[NUM_PARTIDOS];
+	public String arrayPartidos[] = new String[NUM_PARTIDOS];
 	
     public static void main( String[] args )
     {
+    	matchStart();
         
     }
     
@@ -18,13 +21,22 @@ public class Application
     	        
 		for ( int i = 0; i < NUM_PARTIDOS; i++ )
         {
+			Match partido = new Match();
 			for ( int j = 0; j < NUM_SETS; j++ )
 			{
 				
 			}
-        	
         }
 		
 		return "";
+    }
+    
+    public static void calculoJuego()
+    {
+    	//calcularemos el juego
+    	while( (puntoLCL < 4 && puntoVST < 4) || (Math.abs(puntoLCL-puntoVST) < 2) )
+    	{
+    		
+    	}
     }
 }
--- Otras cosas/TennisProject/tennis-project/src/main/java/com/projects/sports/tennis/tennis_project/Match.java
+++ Otras cosas/TennisProject/tennis-project/src/main/java/com/projects/sports/tennis/tennis_project/Match.java
@@ -6,19 +6,8 @@ public class Match {
 	 * Definimos las variables usadas en la clase
 	**/
 	
-	public final int NUM_DE_SETS_POR_PARTIDO = 3; 
-	public Set set[];
-	
-	
-	public Set[] getSet() {
-		return set;
-	}
-	public void setSet(Set[] set) {
-		this.set = set;
-	}
-	public int getNUM_DE_SETS_POR_PARTIDO() {
-		return NUM_DE_SETS_POR_PARTIDO;
-	}
-	
-	
+	public final static int NUM_DE_SETS_POR_PARTIDO = 3; 
+	public final int LCLset = 0;
+	public final int VSTset = 0;
+		
 }
